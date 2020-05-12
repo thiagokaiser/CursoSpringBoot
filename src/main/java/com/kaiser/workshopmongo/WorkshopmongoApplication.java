40f7e7e52cf16c7214bbee2452246981d761d5bb
unknown <thiago.kaiser@hotmail.com>
2020-05-12 15:27:06 -0300

    first commit

---
+++ src/main/java/com/kaiser/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.kaiser.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
