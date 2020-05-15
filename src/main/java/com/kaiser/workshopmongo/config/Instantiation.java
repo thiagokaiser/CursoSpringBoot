e3fc3b55d320af136b5325e3596a075d7b0914ea
unknown <thiago.kaiser@hotmail.com>
2020-05-15 10:13:38 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/kaiser/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kaiser/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.kaiser.workshopmongo.domain.Post;
 import com.kaiser.workshopmongo.domain.User;
+import com.kaiser.workshopmongo.dto.AuthorDTO;
 import com.kaiser.workshopmongo.repository.PostRepository;
 import com.kaiser.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "post1", "bla bla bla", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "post2", "ble ble ble", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "post1", "bla bla bla", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "post2", "ble ble ble", new AuthorDTO(maria));		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
